ec6d94eff2479bf0b21b6397571504684c53a33e
Osvaldo <osvaldovdc12@gmail.com>
2024-10-12 21:42:50 -0300

    commit

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ let buscar = document.getElementById('buscar')
 let pokemon = document.getElementById('pokemon')
 let random
 let tentativas = document.getElementById('tentativas')
+let nomeTentativas = document.getElementById('nomeTentativas')
 let chances 
 let reiniciar = document.getElementById('reiniciar')
 let aBusca = document.getElementById('aBusca')
-
+let nT=[]
 let carregar=()=>{
     random = parseInt((Math.random()*150)+1);
     img.style.filter='brightness(0)'
@@ -25,26 +26,32 @@ let carregar=()=>{
     })            
     reiniciar.style.display='none'
     aBusca.style.display='block'
+    nomeTentativas.style.display='none'
+            nT.length = 0;
 }
-buscar.addEventListener('click',()=>{
+let busca=()=>{
     fetch('https://pokeapi.co/api/v2/pokemon/'+random).then(response=>{
         return response.json()
     })
     .then(data=>{
+            nT.push(pokemon.value)
+            console.log(nT)
         if(pokemon.value==data.name){
             img.style.filter='brightness(1)';
             tentativas.innerHTML=('Parabéns você acertou')
             reiniciar.style.display='block'
             aBusca.style.display='none'
-
         }else{
             chances= chances-1
+            
             tentativas.innerHTML = ('tentativas: '+chances)
             if(chances==0){
                 img.style.filter='brightness(1)';
 
                 tentativas.innerHTML=('Que pena, o pokemon era: '
                      +data.name)
+                     nomeTentativas.innerHTML=('Suas Respostas foram: '+nT)
+                     nomeTentativas.style.display='block'
                      reiniciar.style.display='block'
                      aBusca.style.display='none'
             }
@@ -57,4 +64,14 @@ buscar.addEventListener('click',()=>{
     .catch(error=>{
         console.log(error)
     })
+}
+
+buscar.addEventListener('click',()=>{
+    busca()
 })
+
+let buscarEnter=(event)=>{
+    if(event.key == 'Enter'){
+        busca()
+    }
+}
